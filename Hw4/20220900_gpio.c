a0ebc44cf7bdb2c2692bef59761c5221cc786fa4
Hwang-Sunwoo <167426866+Hwang-Sunwoo@users.noreply.github.com>
2024-05-07 03:34:30 +0900

    Update 20220900_gpio.c

--- Hw4/20220900_gpio.c
+++ Hw4/20220900_gpio.c
@@ -5,21 +5,7 @@
 #define SWITCH_PIN 40  // 입력 스위치에 연결된 GPIO 핀
 int SEGMENT_PINS[8] = {11, 7, 35, 33, 31, 13, 15, 37}  // 7-segment에 연결된 GPIO 핀들
 #define NUM_SEGMENTS 8
-
-void setting() {
-    wiringPiSetupGpio();
-    
-    // 입력 스위치 설정
-    pinMode(SWITCH_PIN, INPUT);
-    digitalWrite(SWITCH_PIN, HIGH);
-
-    // 7-segment 설정
-    int segment_pins[NUM_SEGMENTS] = SEGMENT_PINS;
-    for (int i = 0; i < NUM_SEGMENTS; i++) {
-        pinMode(segment_pins[i], OUTPUT);
-    }
-};
-
+void setting();
 
 int main() {
 
@@ -62,3 +48,17 @@ int main() {
     }
     return 0;
 }
+
+void setting() {
+    wiringPiSetupGpio();
+    
+    // 입력 스위치 설정
+    pinMode(SWITCH_PIN, INPUT);
+    digitalWrite(SWITCH_PIN, HIGH);
+
+    // 7-segment 설정
+    int segment_pins[NUM_SEGMENTS] = SEGMENT_PINS;
+    for (int i = 0; i < NUM_SEGMENTS; i++) {
+        pinMode(segment_pins[i], OUTPUT);
+    }
+};
