ec5b7d3fba0113bf9b398a482fd5e13374088f7d
Hwang-Sunwoo <167426866+Hwang-Sunwoo@users.noreply.github.com>
2024-05-07 03:24:19 +0900

    Update 20220900_gpio.c

--- Hw4/20220900_gpio.c
+++ Hw4/20220900_gpio.c
@@ -6,37 +6,17 @@
 #define SEGMENT_PINS {11, 7, 35, 33, 31, 13, 15, 37}  // 7-segment에 연결된 GPIO 핀들
 #define NUM_SEGMENTS 8
 
-int segmentValues[] = {
-    0b00111111, // 0
-    0b00000110, // 1
-    0b01011011, // 2
-    0b01001111, // 3
-    0b01100110, // 4
-    0b01101101, // 5
-    0b01111101, // 6
-    0b00000111, // 7
-    0b01111111, // 8
-    0b01101111, // 9
-    0b01110111, // A
-    0b01111100, // B
-    0b00111001, // C
-    0b01011110, // D
-    0b01111001, // E
-    0b01110001  // F
-};
-
 void setup() {
     wiringPiSetupGpio();
     
     // 입력 스위치 설정
     pinMode(SWITCH_PIN, INPUT);
-    pullUpDnControl(SWITCH_PIN, PUD_UP);
-    
+    digitalWrite(SWITCH_PIN, HIGH);
+
     // 7-segment 설정
     int segment_pins[NUM_SEGMENTS] = SEGMENT_PINS;
     for (int i = 0; i < NUM_SEGMENTS; i++) {
         pinMode(segment_pins[i], OUTPUT);
-        digitalWrite(segment_pins[i], LOW);
     }
 }
 
@@ -48,10 +28,30 @@ void displayNumber(int number) {
 }
 
 int main() {
-    setup();
-    
+
     int count = 0;
     int btn_state;
+    int b;
+    int sevseq[16][8]={
+	{1,1,1,1,1,1,0,0},
+	{0,1,1,0,0,0,0,0},
+	{1,1,0,1,1,0,1,0},
+	{1,1,1,1,0,0,1,0},
+	{0,1,1,0,0,1,1,0},
+	{1,0,1,1,0,1,1,0},
+	{1,0,1,1,1,1,1,0},
+	{1,1,1,0,0,1,0,0},
+	{1,1,1,1,1,1,1,0},
+	{1,1,1,1,0,1,1,0},
+	{1,1,1,0,1,1,1,0},
+	{0,0,1,1,1,1,1,0},
+	{1,0,0,1,1,1,0,0},
+	{0,1,1,1,1,0,1,0},
+	{1,0,0,1,1,1,1,0},
+	{1,0,0,0,1,1,1,0}
+    };
+    setup();
+    
     
     while(1) {
         btn_state = digitalRead(SWITCH_PIN);
@@ -59,7 +59,9 @@ int main() {
             if (count > 15) {
                 count = 0;
             }
-            displayNumber(count);
+            for(b = 0; b < 8; b++){
+                digitalWrite(SEGMENT_PINS[b], sevseq[count][b]);
+            }
             count++;
             delay(1000);
         }
