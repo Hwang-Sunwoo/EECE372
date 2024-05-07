f5fe34c596ceafa58ec8d30ccccedffd5d299897
Hwang-Sunwoo <167426866+Hwang-Sunwoo@users.noreply.github.com>
2024-05-07 13:38:32 +0900

    Update 20220900_echo_polling.c

--- Hw4/20220900_echo_polling.c
+++ Hw4/20220900_echo_polling.c
@@ -6,8 +6,11 @@
 #include <termios.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <wiringPi.h>
 
 #define BAUDRATE B1000000
+
+int SEGMENT_PINS[8] = {0, 7, 24, 23, 22, 2, 3, 25}  // 7-segment에 연결된 GPIO 핀들
 
 void task()
 {
@@ -21,7 +24,28 @@ int main()
 	struct termios newtio;
 	struct pollfd poll_handler;
 	char buf[256];
-
+    int b;
+    char row;
+    int sevseq[17][8]={
+            {1,1,1,1,1,1,0,0},
+            {0,1,1,0,0,0,0,0},
+            {1,1,0,1,1,0,1,0},
+            {1,1,1,1,0,0,1,0},
+            {0,1,1,0,0,1,1,0},
+            {1,0,1,1,0,1,1,0},
+            {1,0,1,1,1,1,1,0},
+            {1,1,1,0,0,1,0,0},
+            {1,1,1,1,1,1,1,0},
+            {1,1,1,1,0,1,1,0},
+            {1,1,1,0,1,1,1,0},
+            {0,0,1,1,1,1,1,0},
+            {1,0,0,1,1,1,0,0},
+            {0,1,1,1,1,0,1,0},
+            {1,0,0,1,1,1,1,0},
+            {1,0,0,0,1,1,1,0},
+            {0,1,1,0,1,1,1,0}
+        };
+    
 	fd = open("/dev/serial0", O_RDWR|O_NOCTTY);
 	if(fd<0) {
 		fprintf(stderr, "failed to open port: %s.\r\n", strerror(errno));
@@ -50,6 +74,12 @@ int main()
 
 	write(fd, "Polling method\r\n", 16);
 
+    wiringPiSetupGpio();
+    pinMode(SWITCH_PIN, INPUT); // 입력 스위치 설정
+    for (int i = 0; i < 8; i++) { // 7-segment 설정
+        pinMode(SEGMENT_PINS[i], OUTPUT);
+    }
+    
 	while(1) {
 		task();
 		if(poll((struct pollfd*)&poll_handler, 1, 2000) > 0) {
@@ -59,6 +89,22 @@ int main()
 				write(fd, "echo: ", 6);
 				write(fd, buf, cnt);
 				write(fd, "\r\n", 2);
+                row = char(buf[0]);
+                
+                if(48 <= row && row <= 57){
+                    for(b = 0; b < 8; b++){
+                        digitalWrite(SEGMENT_PINS[b], sevseq[row - 48][b]);
+                    }
+                }else if(65 <= row && row <= 70){
+                    
+                    for(b = 0; b < 8; b++){
+                        digitalWrite(SEGMENT_PINS[b], sevseq[row - 55][b]);
+                    }
+                }else{
+                    for(b = 0; b < 8; b++){
+                        digitalWrite(SEGMENT_PINS[b], sevseq[16][b]);
+                    }
+                }
 			}
 			else if(poll_handler.revents & POLLERR) {
 				printf("Error in communication. Abort program\r\n");
