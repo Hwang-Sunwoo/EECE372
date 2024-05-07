26d817cbcbd77e22a6344f84209b76332e44bfa4
Hwang-Sunwoo <167426866+Hwang-Sunwoo@users.noreply.github.com>
2024-05-07 21:04:12 +0900

    Update 20220900_echo_polling.c

--- Hw4/20220900_echo_polling.c
+++ Hw4/20220900_echo_polling.c
@@ -10,14 +10,7 @@
 
 #define BAUDRATE B1000000
 
-#define A 0
-#define B 7
-#define C 24
-#define D 23
-#define E 22
-#define F 2
-#define G 3
-#define DP 25
+int SEGMENT_PINS[8] = {0, 7, 3, 22, 23, 24, 25, 2};  // 7-segment에 연결된 GPIO 핀들
 
 void task()
 {
@@ -25,12 +18,31 @@ void task()
 	for(i=0; i<400000000; i++);
 }
 
-int main()
-{
-	int fd;
-	struct termios newtio;
-	struct pollfd poll_handler;
-	char buf[256];
+int main(){
+    int fd;
+    struct termios newtio;
+    struct pollfd poll_handler;
+    char buf[256];
+    char row;
+    int sev_seg[17][8]={
+	{1,1,1,1,1,1,0,0},
+	{0,1,1,0,0,0,0,0},
+	{1,1,0,1,1,0,1,0},
+	{1,1,1,1,0,0,1,0},
+	{0,1,1,0,0,1,1,0},
+	{1,0,1,1,0,1,1,0},
+	{1,0,1,1,1,1,1,0},
+	{1,1,1,0,0,1,0,0},
+	{1,1,1,1,1,1,1,0},
+	{1,1,1,1,0,1,1,0},
+	{1,1,1,0,1,1,1,0},
+	{0,0,1,1,1,1,1,0},
+	{0,0,0,1,1,0,1,0},
+	{0,1,1,1,1,0,1,0},
+	{1,0,0,1,1,1,1,0},
+	{1,0,0,0,1,1,1,0},
+	{0,1,1,0,1,1,1,0}
+    };
 
 	fd = open("/dev/serial0", O_RDWR|O_NOCTTY);
 	if(fd<0) {
@@ -59,274 +71,45 @@ int main()
 	poll_handler.revents = 0;
 
 	write(fd, "Polling method\r\n", 16);
-	
 
-	if (wiringPiSetup() == -1){
-		return 1;
-	}
+    if (wiringPiSetup() == -1){
+	return 1;
+    }
 	
-	pinMode(A, OUTPUT);
-	pinMode(B, OUTPUT);
-	pinMode(C, OUTPUT);
-	pinMode(D, OUTPUT);
-	pinMode(E, OUTPUT);
-	pinMode(F, OUTPUT);
-	pinMode(G, OUTPUT);
-	pinMode(DP, OUTPUT);
-	
-	
-	while(1) {
-		task();
-		if(poll((struct pollfd*)&poll_handler, 1, 2000) > 0) {
-			if(poll_handler.revents & POLLIN) {
-				int cnt = read(fd, buf, sizeof(buf));
-				buf[cnt] = '\0';
-				write(fd, "echo: ", 6);
-				write(fd, buf, cnt);
-				write(fd, "\r\n", 2);
-				
-				
-				switch(buf[0]){
-					case '0':
-						digitalWrite(A, 1);
-						digitalWrite(B, 1);
-						digitalWrite(C, 1);
-						digitalWrite(D, 1);
-						digitalWrite(E, 1);
-						digitalWrite(F, 1);
-						digitalWrite(G, 0);
-						digitalWrite(DP, 0);
-						break;
-					case '1':
-						digitalWrite(A, 0);
-						digitalWrite(B, 1);
-						digitalWrite(C, 1);
-						digitalWrite(D, 0);
-						digitalWrite(E, 0);
-						digitalWrite(F, 0);
-						digitalWrite(G, 0);
-						digitalWrite(DP, 0);
-						break;
-					case '2':
-						digitalWrite(A, 1);
-						digitalWrite(B, 1);
-						digitalWrite(C, 0);
-						digitalWrite(D, 1);
-						digitalWrite(E, 1);
-						digitalWrite(F, 0);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case '3':
-						digitalWrite(A, 1);
-						digitalWrite(B, 1);
-						digitalWrite(C, 1);
-						digitalWrite(D, 1);
-						digitalWrite(E, 0);
-						digitalWrite(F, 0);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case '4':
-						digitalWrite(A, 0);
-						digitalWrite(B, 1);
-						digitalWrite(C, 1);
-						digitalWrite(D, 0);
-						digitalWrite(E, 0);
-						digitalWrite(F, 1);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case '5':
-						digitalWrite(A, 1);
-						digitalWrite(B, 0);
-						digitalWrite(C, 1);
-						digitalWrite(D, 1);
-						digitalWrite(E, 0);
-						digitalWrite(F, 1);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case '6':
-						digitalWrite(A, 1);
-						digitalWrite(B, 0);
-						digitalWrite(C, 1);
-						digitalWrite(D, 1);
-						digitalWrite(E, 1);
-						digitalWrite(F, 1);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case '7':
-						digitalWrite(A, 1);
-						digitalWrite(B, 1);
-						digitalWrite(C, 1);
-						digitalWrite(D, 0);
-						digitalWrite(E, 0);
-						digitalWrite(F, 1);
-						digitalWrite(G, 0);
-						digitalWrite(DP, 0);
-						break;
-					case '8':
-						digitalWrite(A, 1);
-						digitalWrite(B, 1);
-						digitalWrite(C, 1);
-						digitalWrite(D, 1);
-						digitalWrite(E, 1);
-						digitalWrite(F, 1);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case '9':
-						digitalWrite(A, 1);
-						digitalWrite(B, 1);
-						digitalWrite(C, 1);
-						digitalWrite(D, 1);
-						digitalWrite(E, 0);
-						digitalWrite(F, 1);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case 'a':
-						digitalWrite(A, 1);
-						digitalWrite(B, 1);
-						digitalWrite(C, 1);
-						digitalWrite(D, 0);
-						digitalWrite(E, 1);
-						digitalWrite(F, 1);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case 'b':
-						digitalWrite(A, 0);
-						digitalWrite(B, 0);
-						digitalWrite(C, 1);
-						digitalWrite(D, 1);
-						digitalWrite(E, 1);
-						digitalWrite(F, 1);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case 'c':
-						digitalWrite(A, 0);
-						digitalWrite(B, 0);
-						digitalWrite(C, 0);
-						digitalWrite(D, 1);
-						digitalWrite(E, 1);
-						digitalWrite(F, 0);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case 'd':
-						digitalWrite(A, 0);
-						digitalWrite(B, 1);
-						digitalWrite(C, 1);
-						digitalWrite(D, 1);
-						digitalWrite(E, 1);
-						digitalWrite(F, 0);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case 'e':
-						digitalWrite(A, 1);
-						digitalWrite(B, 0);
-						digitalWrite(C, 0);
-						digitalWrite(D, 1);
-						digitalWrite(E, 1);
-						digitalWrite(F, 1);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case 'f':
-						digitalWrite(A, 1);
-						digitalWrite(B, 0);
-						digitalWrite(C, 0);
-						digitalWrite(D, 0);
-						digitalWrite(E, 1);
-						digitalWrite(F, 1);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case 'A':
-						digitalWrite(A, 1);
-						digitalWrite(B, 1);
-						digitalWrite(C, 1);
-						digitalWrite(D, 0);
-						digitalWrite(E, 1);
-						digitalWrite(F, 1);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case 'B':
-						digitalWrite(A, 0);
-						digitalWrite(B, 0);
-						digitalWrite(C, 1);
-						digitalWrite(D, 1);
-						digitalWrite(E, 1);
-						digitalWrite(F, 1);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case 'C':
-						digitalWrite(A, 0);
-						digitalWrite(B, 0);
-						digitalWrite(C, 0);
-						digitalWrite(D, 1);
-						digitalWrite(E, 1);
-						digitalWrite(F, 0);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case 'D':
-						digitalWrite(A, 0);
-						digitalWrite(B, 1);
-						digitalWrite(C, 1);
-						digitalWrite(D, 1);
-						digitalWrite(E, 1);
-						digitalWrite(F, 0);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case 'E':
-						digitalWrite(A, 1);
-						digitalWrite(B, 0);
-						digitalWrite(C, 0);
-						digitalWrite(D, 1);
-						digitalWrite(E, 1);
-						digitalWrite(F, 1);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					case 'F':
-						digitalWrite(A, 1);
-						digitalWrite(B, 0);
-						digitalWrite(C, 0);
-						digitalWrite(D, 0);
-						digitalWrite(E, 1);
-						digitalWrite(F, 1);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-					default:
-						digitalWrite(A, 0);
-						digitalWrite(B, 1);
-						digitalWrite(C, 1);
-						digitalWrite(D, 0);
-						digitalWrite(E, 1);
-						digitalWrite(F, 1);
-						digitalWrite(G, 1);
-						digitalWrite(DP, 0);
-						break;
-				}
-				
-				
+   for(int i = 0; i < 8; i++){
+	pinMode(SEGMENT_PINS[i], OUTPUT);
+   }
+    
+    while(1) {
+	task();
+	if(poll((struct pollfd*)&poll_handler, 1, 2000) > 0) {
+		if(poll_handler.revents & POLLIN) {
+			int cnt = read(fd, buf, sizeof(buf));
+			buf[cnt] = '\0';
+			write(fd, "echo: ", 6);
+			write(fd, buf, cnt);
+			write(fd, "\r\n", 2);
+        		row = (char)buf[0];
+			
+			if(48 <= row && row <= 57){
+				for(int i = 0; i < 8; i++)
+					digitalWrite(SEGMENT_PINS[i], sev_seg[row - 48][i]);
 			}
-			else if(poll_handler.revents & POLLERR) {
-				printf("Error in communication. Abort program\r\n");
-				return 0;
+			else if(65 <= row && row <= 70){
+				for(int i = 0; i < 8; i++)
+					digitalWrite(SEGMENT_PINS[i], sev_seg[row - 55][i]);
 			}
+			else{
+				for(int i = 0; i < 8; i++)
+					digitalWrite(SEGMENT_PINS[i], sev_seg[16][i]);
+			}
+
+		}
+		else if(poll_handler.revents & POLLERR) {
+			printf("Error in communication. Abort program\r\n");
+			return 0;
 		}
 	}
-
+    }
 	return 0;
 }
