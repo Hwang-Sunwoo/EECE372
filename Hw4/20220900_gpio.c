ffc8f3dae9772854740c967b2db588453dfcf0f0
Hwang-Sunwoo <167426866+Hwang-Sunwoo@users.noreply.github.com>
2024-05-07 13:43:26 +0900

    Update 20220900_gpio.c

--- Hw4/20220900_gpio.c
+++ Hw4/20220900_gpio.c
@@ -3,7 +3,7 @@
 #include <wiringPi.h>
 
 #define SWITCH_PIN 29  // 입력 스위치에 연결된 GPIO 핀
-int SEGMENT_PINS[8] = {0, 7, 24, 23, 22, 2, 3, 25}  // 7-segment에 연결된 GPIO 핀들
+int SEGMENT_PINS[8] = {0, 7, 24, 23, 22, 2, 3, 25};  // 7-segment에 연결된 GPIO 핀들
 
 
 int main() {
@@ -41,13 +41,13 @@ int main() {
     
     while(1) {
         if(btn_state == 0){
-            if(digitalRead(SWITCH_PINS) == 1){
+            if(digitalRead(SWITCH_PIN) == 1){
                 count++;
                 count = count % 16;
                 btn_state = 1;
             }
         }else if(btn_state == 1){
-            if(digitalRead(SWITCH_PINS) == 0){
+            if(digitalRead(SWITCH_PIN) == 0){
                 btn_state = 0;
             }
         }
