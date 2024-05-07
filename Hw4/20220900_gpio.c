c967402d36fc59fbd9417634d5466bb7d3a12769
Hwang-Sunwoo <167426866+Hwang-Sunwoo@users.noreply.github.com>
2024-05-07 11:30:58 +0900

    Update 20220900_gpio.c

--- Hw4/20220900_gpio.c
+++ Hw4/20220900_gpio.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <wiringPi.h>
 
-#define SWITCH_PIN 40  // 입력 스위치에 연결된 GPIO 핀
-int SEGMENT_PINS[8] = {11, 7, 35, 33, 31, 13, 15, 37}  // 7-segment에 연결된 GPIO 핀들
-#define NUM_SEGMENTS 8
+#define SWITCH_PIN 29  // 입력 스위치에 연결된 GPIO 핀
+int SEGMENT_PINS[8] = {0, 7, 24, 23, 22, 2, 3, 25}  // 7-segment에 연결된 GPIO 핀들
+
 void setting();
 
 int main() {
 
     int count = 0;
-    int btn_state;
+    int btn_state = 0;
     int b;
     int sevseq[16][8]={
 	{1,1,1,1,1,1,0,0},
@@ -30,20 +30,28 @@ int main() {
 	{1,0,0,1,1,1,1,0},
 	{1,0,0,0,1,1,1,0}
     };
+    
     setting();
     
     
     while(1) {
-        btn_state = digitalRead(SWITCH_PIN);
-        if (btn_state == HIGH) {
-            if (count > 15) {
-                count = 0;
+        if(btn_state == 0){
+            if(digitalRead(SWITCH_PINS) == 1){
+                count++;
+                count = count % 16;
+                btn_state = 1;
             }
+        }else if(btn_state == 1){
+            if(digitalRead(SWITCH_PINS) == 0){
+                btn_state = 0;
+            }
+
+        }
+
             for(b = 0; b < 8; b++){
                 digitalWrite(SEGMENT_PINS[b], sevseq[count][b]);
             }
-            count++;
-            delay(1000);
+            delay(100);
         }
     }
     return 0;
@@ -52,13 +60,9 @@ int main() {
 void setting() {
     wiringPiSetupGpio();
     
-    // 입력 스위치 설정
-    pinMode(SWITCH_PIN, INPUT);
-    digitalWrite(SWITCH_PIN, HIGH);
+    pinMode(SWITCH_PIN, INPUT); // 입력 스위치 설정
 
-    // 7-segment 설정
-    int segment_pins[NUM_SEGMENTS] = SEGMENT_PINS;
-    for (int i = 0; i < NUM_SEGMENTS; i++) {
-        pinMode(segment_pins[i], OUTPUT);
+    for (int i = 0; i < 8; i++) { // 7-segment 설정
+        pinMode(SEGMENT_PINS[i], OUTPUT);
     }
 };
