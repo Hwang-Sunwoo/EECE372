ee0a6b160570710da260ef403dc6ad287008e97e
Hwang-Sunwoo <167426866+Hwang-Sunwoo@users.noreply.github.com>
2024-05-21 11:53:35 +0900

    Update 20220900_camera.c

--- Hw5/20220900_camera.c
+++ Hw5/20220900_camera.c
@@ -39,8 +39,26 @@ int main()
 	tcsetattr(fd, TCSANOW, &newtio);
 
 	while(1) {
-
 		// Insert your code
+		memset(fbuf, 0 sizeof(fbuf));
+		int cnt = read(fd, buf, sizeof(buf));
+		buf[cnt] = '\0';
+	
+		if(buf[0] == 'c' || buf[0] == 'C'){
+			printf("kimch\r\n");
+			system("libcamera-still --width 640 --height 480 -o image.jpg");
+			
+			FILE *fp = fopen("image.jpg", "rb");
+			if(fp == NULL){
+				fprintf(stderr, "failed to open image file: %s.\r\n", strerror(errno));
+				continue;
+			}
+			for (int i = 0; i < 480; i++){
+				fread(fbuf, 1, 640, fp);
+				write(fd, fbuf, 640);
+			}
+			fclose(fp);		
+		}
 
 	}
 	return 0;
