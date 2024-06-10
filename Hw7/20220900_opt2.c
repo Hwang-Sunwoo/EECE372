7eb40e875c27ad9346866c8f29da829e03c4454d
Hwang-Sunwoo <167426866+Hwang-Sunwoo@users.noreply.github.com>
2024-06-11 03:46:09 +0900

    Update 20220900_opt2.c

--- Hw7/20220900_opt2.c
+++ Hw7/20220900_opt2.c
@@ -319,30 +319,27 @@ void Conv_2d(float *feature_in, float *feature_out, int in_C, int in_H, int in_W
 }
 void ReLU(float *feature_in, int elem_num) {
     asm(
-        "push {r4, lr}\n\t"             // 레지스터 r4와 lr을 스택에 푸시하여 저장
-        "mov r2, #0\n\t"                // r2를 0으로 초기화 (배열의 인덱스로 사용)
+	"mov r2, #0\n\t"                // r2를 0으로 초기화 (배열의 인덱스로 사용)
 
         "loop_start:\n\t"
-        "cmp r2, r1\n\t"                // r2와 r1을 비교 (현재 인덱스와 배열의 길이를 비교)
+        "cmp r2, %[elem_num]\n\t"                // r2와 r1을 비교 (현재 인덱스와 배열의 길이를 비교)
         "beq loop_end\n\t"              // r2가 r1과 같으면 루프를 종료
 
-        "ldr r3, [r0, r2, LSL #2]\n\t"  // r0 + r2 * 4에서 값을 로드하여 r3에 저장 (현재 배열 요소)
+        "ldr r3, [%[feature_in], r2, LSL #2]\n\t"  // r0 + r2 * 4에서 값을 로드하여 r3에 저장 (현재 배열 요소)
+	"ldr r4, [r3]\n\t"
         "cmp r3, #0\n\t"                // r3와 0을 비교
         "bge no_change\n\t"             // r3가 0보다 크거나 같으면 값을 변경하지 않음
 
-        "mov r3, #0\n\t"                // r3를 0으로 설정
-        "str r3, [r0, r2, LSL #2]\n\t"  // r0 + r2 * 4에 r3를 저장 (변경된 배열 요소)
+        "str #0, [r3]\n\t"  // r0 + r2 * 4에 r3를 저장 (변경된 배열 요소)
 
         "no_change:\n\t"
         "add r2, r2, #1\n\t"            // r2를 1 증가 (다음 인덱스로 이동)
         "b loop_start\n\t"              // 루프 시작으로 돌아감
 
         "loop_end:\n\t"
-        "pop {r4, lr}\n\t"              // 레지스터 r4와 lr을 스택에서 팝하여 복원
-        "bx lr\n\t"                     // lr로 복귀
         :
         : [feature_in] "r"(feature_in), [elem_num] "r"(elem_num)
-        : "r0", "r1", "r2", "r3", "r4", "memory"
+        : "r2", "r3", "r4", "memory"
     );
 }
 
