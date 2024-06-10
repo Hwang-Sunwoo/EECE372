bdd67eb6260a74af3387b7a1a29fba909ef0aba9
Hwang-Sunwoo <167426866+Hwang-Sunwoo@users.noreply.github.com>
2024-06-11 02:10:22 +0900

    Update 20220900_opti.c

--- Hw7/20220900_opti.c
+++ Hw7/20220900_opti.c
@@ -442,8 +442,7 @@ void Padding(float *feature_in, float *feature_out, int C, int H, int W) {
         : "r0", "r1", "r2", "r3", "r4", "r5", "memory"
     );
 }
-#include <arm_neon.h>
-
+/*
 void Conv_2d(float *feature_in, float *feature_out, int in_C, int in_H, int in_W, int out_C, int out_H, int out_W, int K, int S, float *weight, float *bias) {
     for (int oc = 0; oc < out_C; oc++) {
         for (int oh = 0; oh < out_H; oh++) {
@@ -480,6 +479,34 @@ void Conv_2d(float *feature_in, float *feature_out, int in_C, int in_H, int in_W
                     }
                 }
                 feature_out[oc * out_H * out_W + oh * out_W + ow] = sum;
+            }
+        }
+    }
+}*/
+
+void Conv_2d(float *feature_in, float *feature_out, int in_C, int in_H, int in_W, int out_C, int out_H, int out_W, int K, int S, float *weight, float *bias) {
+    /*          PUT YOUR CODE HERE          */
+    // Conv_2d input : float *feature_in
+    // Conv_2d output: float *feature_out
+    #pragma omp parallel for collapse(3)
+    for (int oc = 0; oc < out_C; oc++) {
+        for (int oh = 0; oh < out_H; oh++) {
+            for (int ow = 0; ow < out_W; ow++) {
+                float32x4_t sum_vec = vdupq_n_f32(bias[oc]);
+                for (int ic = 0; ic < in_C; ic++) {
+                    for (int kh = 0; kh < K; kh++) {
+                        for (int kw = 0; kw < K; kw++) {
+                            int ih = oh * S + kh;
+                            int iw = ow * S + kw;
+                            float *in_ptr = feature_in + ic * in_H * in_W + ih * in_W + iw;
+                            float *w_ptr = weight + oc * in_C * K * K + ic * K * K + kh * K + kw;
+                            float32x4_t in_vec = vld1q_f32(in_ptr);
+                            float32x4_t w_vec = vld1q_f32(w_ptr);
+                            sum_vec = vmlaq_f32(sum_vec, in_vec, w_vec);
+                        }
+                    }
+                }
+                feature_out[oc * out_H * out_W + oh * out_W + ow] = vgetq_lane_f32(sum_vec, 0) + vgetq_lane_f32(sum_vec, 1) + vgetq_lane_f32(sum_vec, 2) + vgetq_lane_f32(sum_vec, 3);
             }
         }
     }
